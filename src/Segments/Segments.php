bacae4840756d6ca816fa04851335006e626ed64
Diego Sorribas <diego.sorribas@solcre.com>
2019-11-12 11:58:19 -0300

    refactor(Segments): remove query params

--- src/Segments/Segments.php
+++ src/Segments/Segments.php
@@ -23,9 +23,7 @@ class Segments extends Resource
     {
         $req = $this->client
             ->getClient()
-            ->get('/api/3/segments/' . $id);
-
-        return $req->getBody()->getContents();
+            ->get('/api/3/segments/' . $id)->getBody()->getContents();
     }
 
     /**
@@ -37,20 +35,11 @@ class Segments extends Resource
      * @param  $offset
      * @return string
      */
-    public function listAll(array $query_params = [], $limit = 20, $offset = 0)
+    public function listAll()
     {
-        $query_params = array_merge($query_params, [
-            'limit'  => $limit,
-            'offset' => $offset
-        ]);
-
-        $req = $this->client
+        return $this->client
             ->getClient()
-            ->get('/api/3/segments', [
-                'query' => $query_params
-            ]);
-
-        return $req->getBody()->getContents();
+            ->get('/api/3/segments')->getBody()->getContents();
     }
 
 }
